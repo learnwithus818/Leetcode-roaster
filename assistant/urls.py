e08340d1fb93bfd7e9257b7410e61c0249f847b8
Anurag Upadhyay <anuragfx818@gmail.com>
2024-07-06 14:13:22 +0530

    env added

--- assistant/urls.py
+++ assistant/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('audioindex', views.audioindex, name='audioindex'),
     path('home', views.home, name='home'),
-    path('process_audio/', views.process_audio, name='process_audio'),
 ]
--- assistantApp/settings.py
+++ assistantApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kf=o=my=09lde-4%%t$w)!c=j6a8i0py5#t_fxf=*k*)p=(g_#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", '.vercel.app']
 
 
 # Application definition
